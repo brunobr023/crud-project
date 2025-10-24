8f0e1b1973af442846b44cbd78778bf400b55a70
brunobr023 <brunootavio326@gmail.com>
2025-10-24 12:11:25 -0300

    Fazendo a conexão do banco de dados e isntalação do axios.

---
+++ backend/src/main/java/com/crudzin/backend/controler.java
@@ -0,0 +1,9 @@
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/api/teste")
+public class TesteController {
+    @GetMapping
+    public String testar() {
+        return "Backend funcionando!";
+    }
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import { useEffect, useState } from 'react';
+import { api } from './services/api.ts';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mensagem, setMensagem] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    api.get('/teste').then(res => setMensagem(res.data));
+  }, []);
+
+  return <h1>{mensagem}</h1>;
 }
 
-export default App
+export default App;
