fe0d37cc6e166658d816b48daa3272cc3dfcf975
vitin <vitinhoc2005@gmail.com>
2025-10-31 13:10:53 -0300

    Ajeitando Backend

--- backend/src/main/java/com/crudzin/backend/TesteController.java
+++ backend/src/main/java/com/crudzin/backend/TesteController.java
@@ -1,9 +1,14 @@
-// @CrossOrigin(origins = "http://localhost:5173")
-// @RestController
-// @RequestMapping("/api/teste")
-// public class TesteController {
-//     @GetMapping
-//     public String testar() {
-//         return "Backend funcionando!";
-//     }
-// }
+package com.crudzin.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+ @RestController
+ @RequestMapping("/")
+ public class TesteController {
+     @GetMapping
+     public String testar() {
+         return "Backend funcionando!";
+     }
+ }
