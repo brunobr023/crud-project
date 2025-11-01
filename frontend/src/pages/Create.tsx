b42d93a18f35e345a86c7505f9e233ed4b400a4b
brunobr023 <brunootavio326@gmail.com>
2025-11-01 00:09:02 -0300

    Finalizando a base...

--- frontend/src/pages/Create.tsx
+++ frontend/src/pages/Create.tsx
@@ -2,15 +2,6 @@ import React, { useState } from 'react';
 import './css/Create.css';
 
 
-const BotaoVoltar = () => {
-  return (
-    <button className="icon-button">
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="#D3FF32">
-        <path xmlns="http://www.w3.org/2000/svg" d="M669.6 849.6c8.8 8 22.4 7.2 30.4-1.6s7.2-22.4-1.6-30.4l-309.6-280c-8-7.2-8-17.6 0-24.8l309.6-270.4c8.8-8 9.6-21.6 2.4-30.4-8-8.8-21.6-9.6-30.4-2.4L360.8 480.8c-27.2 24-28 64-0.8 88.8l309.6 280z" fill=""/>
-      </svg>
-    </button>
-  );
-};
 interface FormData{
   nome: string;
   email: string;
@@ -48,7 +39,9 @@ const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <div className='menu'>
-            <BotaoVoltar />
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none">
+              <path d="M15 18l-6-6 6-6" stroke="#3A29FF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
             <h1>Formulário de Criação de Usuários</h1>
           </div>
           <label htmlFor="nome">Nome:</label>
